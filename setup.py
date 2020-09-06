4eb288ff038fe2a3a6eeeae33e58158aafa91d6d
Felix Mertens <felix.mertens@web.de>
2020-09-06 19:19:13 +0200

    package

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="test_package", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
+    author="Felix Mertens",
+    author_email="felix.mertens@web.de",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
